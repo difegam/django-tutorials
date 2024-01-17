139c4c54be3a9d5872ce7971d2db09ce25fe98fe
difegam <difegam3@gmail.com>
2024-01-17 22:29:02 +0100

    Add PostAdmin class to admin.py

--- django-by-example/dgamboa-blog/dgamboa_blog/blog/admin.py
+++ django-by-example/dgamboa-blog/dgamboa_blog/blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Post
+
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "slug",
+        "author",
+        "publish",
+        "status",
+    )  # Display fields in admin
+    list_filter = ("status", "created", "publish", "author")  # Filter in sidebar
+    search_fields = ("title", "body")  # Search by title and body
+    prepopulated_fields = {
+        "slug": ("title",)
+    }  # Slug field will be filled automatically
+    raw_id_fields = ("author",)  # Lookup author instead of dropdown 
+    date_hierarchy = "publish"  # Generate navigation by date
+    ordering = ("status", "publish")
