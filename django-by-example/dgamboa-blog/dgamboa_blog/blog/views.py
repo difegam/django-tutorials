2b8333f4e6abfa75bf16d5ebd15e40156aaf5f50
difegam <difegam3@gmail.com>
2024-01-17 22:29:13 +0100

    Add post_list and post_detail views

--- django-by-example/dgamboa-blog/dgamboa_blog/blog/views.py
+++ django-by-example/dgamboa-blog/dgamboa_blog/blog/views.py
@@ -1,3 +1,18 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request) -> HttpResponse:
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request, id: int) -> HttpResponse:
+    """Retrieve a published post by id"""
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist as error:
+        raise Http404("Post does not exist") from error
+    return render(request, "blog/post/detail.html", {"post": post})
