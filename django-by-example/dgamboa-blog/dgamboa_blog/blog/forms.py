5135a317dead759184880688601814671207b1ca
difegam <difegam3@gmail.com>
2024-02-04 17:39:15 +0100

    Add CommentForm to blog/forms.py

--- django-by-example/dgamboa-blog/dgamboa_blog/blog/forms.py
+++ django-by-example/dgamboa-blog/dgamboa_blog/blog/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from .models import Comment
+
+
 class EmailPostForm(forms.Form):
     """Form to share a post by email"""
 
@@ -8,4 +11,15 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField(label="Recipient email")
     comments = forms.CharField(required=False, widget=forms.Textarea, label="Comments")
 
-    
+
+class CommentForm(forms.ModelForm):
+    """Form to create a comment"""
+
+    class Meta:
+        model = Comment
+        fields = ("name", "email", "body")
+        labels = (
+            ("name", "Your name"),
+            ("email", "Your email"),
+            ("body", "Your comment"),
+        )
