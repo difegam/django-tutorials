9a60d95879b6bb8f0f06fec43c51e68837e3ed47
difegam <difegam3@gmail.com>
2024-06-20 14:23:43 +0200

    chore: Update database configuration in settings.py

--- django-by-example/dgamboa-blog/dgamboa_blog/dgamboa/settings.py
+++ django-by-example/dgamboa-blog/dgamboa_blog/dgamboa/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
     "default": {
         "ENGINE": env.str("SQL_ENGINE", default="django.db.backends.sqlite3"),
         "NAME": env.str("SQL_DATABASE", default=BASE_DIR / "db.sqlite3"),
-        "USER": env.str("SQL_USER", default="user"),
-        "PASSWORD": env.str("SQL_PASSWORD", default="password"),
+        "USER": env.str("SQL_USER"),
+        "PASSWORD": env.str("SQL_PASSWORD"),
         "HOST": env.str("SQL_HOST", default="localhost"),
         "PORT": env.str("SQL_PORT", default="5432"),
     },
